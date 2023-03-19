f32be9bcc86ca291a05bbf761069b384e0b5a2fd
srikanth9943 <126959638+srikanth9943@users.noreply.github.com>
2023-03-19 17:32:24 +0530

    lead feature (#1)
    complete
    *update lead and get lead funtionality done

---
+++ controller/lead.py
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+=======
+# Lead controller implementation.
+
+
+# Create lead.
+def create_lead():
+  print 'Lead creation done.'
+
+# Update lead.
+def update_lead():
+  print 'Lead update done.'
+
+# Get lead.
+def get_lead():
+  print 'Get lead done.'
+  
+>>>>>>> 2fd1438 (lead feature in progress)
---
+++ model/lead.py
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+=======
+# Lead model implementation.
+
+
+# Insert lead query.
+def insert_lead_query():
+  print 'Lead inserted into database.'
+
+# Update lead query.
+def update_lead_query():
+  print 'Lead updated into database.'
+
+# Get lead query.
+def select_lead_query():
+  print 'Lead select from database.'
+>>>>>>> 2fd1438 (lead feature in progress)
